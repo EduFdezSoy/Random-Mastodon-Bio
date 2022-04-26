b12991d878340ac598485cd628b0da40e7ae73ec
Eduardo Fernandez <yo@edufdez.es>
2022-04-27 01:45:17 +0200

    this is it

---
+++ change_bio.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+use Fundevogel\Mastodon\Api;
+
+
+// mastodon auth things
+const INSTANCE = 'mastodon.social';
+const ACCESS_TOKEN = 'your_access_token_here';
+
+// where are the sentences?
+$biosFile = 'bios';
+
+// load the file and pick a random one
+$file = file($biosFile);
+$randIndex = array_rand($file);
+$description = $file[$randIndex];
+
+// set credentials
+$api = new Api(INSTANCE);
+$api->accessToken = ACCESS_TOKEN;
+
+// login
+$api->logIn();
+
+// retrive profile data
+$accData = $api->accounts()->verifyCredentials();
+
+// put new profile data
+$res = $api->accounts()->updateCredentials(
+  $discoverable     = $accData->discoverable(),
+  $bot              = $accData->bot(),
+  $displayName      = $accData->displayName(),
+  $note             = $description,
+  $avatar           = '',
+  $header           = '',
+  $locked           = $accData->locked(),
+  $sourcePrivacy    = $accData->source()->privacy(),
+  $sourceSensitive  = $accData->source()->sensitive(),
+  $fieldAttributes  = "",
+);
+var_dump($res);
